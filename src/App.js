0fe9342641b51db09835f5c68dcf80a721a4310a
Joes-BitGit <josephnalmeida@gmail.com>
2021-09-03 17:28:29 -0700

    refactored to include useReducer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,141 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 
 function App() {
   const defaultSession = 25;
   const defaultBreak = 5;
   const minuteMultiplier = 60;
-  const [session, setSession] = useState(5);
-  const [breaktime, setBreaktime] = useState(5);
-  const [timeSeconds, setTimeSeconds] = useState(session * minuteMultiplier);
-  const [breakTimeSeconds, setBreakTimeSeconds] = useState(
-    breaktime * minuteMultiplier
+
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "INC_SESSION": {
+          return {
+            ...state,
+            session: state.session + 1,
+            // will this get prev state or does it read the previous statement first?
+            timeSeconds: state.timeSeconds + minuteMultiplier,
+          };
+        }
+        case "SESSION_OVERFLOW": {
+          return {
+            ...state,
+            session: 60,
+          };
+        }
+        case "DEC_SESSION": {
+          return {
+            ...state,
+            session: state.session - 1,
+            // will this get prev state or does it read the previous statement first?
+            timeSeconds: state.timeSeconds - minuteMultiplier,
+          };
+        }
+        case "SESSION_UNDERFLOW": {
+          return {
+            ...state,
+            session: 1,
+          };
+        }
+        case "INC_BREAK": {
+          return {
+            ...state,
+            breaktime: state.breaktime + 1,
+            // will this get prev state or does it read the previous statement first?
+            breakTimeSeconds: state.breakTimeSeconds + minuteMultiplier,
+          };
+        }
+        case "BREAK_OVERFLOW": {
+          return {
+            ...state,
+            session: 60,
+          };
+        }
+        case "DEC_BREAK": {
+          return {
+            ...state,
+            breaktime: state.breaktime - 1,
+            // will this get prev state or does it read the previous statement first?
+            breakTimeSeconds: state.breakTimeSeconds - minuteMultiplier,
+          };
+        }
+        case "BREAK_UNDERFLOW": {
+          return {
+            ...state,
+            session: 1,
+          };
+        }
+        case "TOGGLE_PLAY_PAUSE": {
+          return {
+            ...state,
+            isActive: !state.isActive,
+          };
+        }
+        case "SESSION_RUNNING": {
+          return {
+            ...state,
+            sessionState: true,
+            breakState: false,
+            timeSeconds: state.timeSeconds - 1,
+          };
+        }
+        case "BREAK_RUNNING": {
+          return {
+            ...state,
+            breakState: true,
+            sessionState: false,
+            breakTimeSeconds: state.breakTimeSeconds - 1,
+          };
+        }
+        case "SESSION_BREAK": {
+          return {
+            ...state,
+            sessionState: !state.sessionState,
+            breakState: !state.breakState,
+            timeSeconds: state.session * minuteMultiplier,
+          };
+        }
+        case "BREAK_SESSION": {
+          return {
+            ...state,
+            sessionState: !state.sessionState,
+            breakState: !state.breakState,
+            breakTimeSeconds: state.breaktime * minuteMultiplier,
+          };
+        }
+        case "RESET": {
+          return {
+            session: defaultSession,
+            breaktime: defaultBreak,
+            isActive: false,
+            timeSeconds: defaultSession * minuteMultiplier,
+            breakTimeSeconds: defaultBreak * minuteMultiplier,
+            sessionState: true,
+            breakState: false,
+          };
+        }
+        default:
+          return state;
+      }
+    },
+    {
+      // intial state of my application
+      session: 1,
+      breaktime: 1,
+      isActive: false,
+      timeSeconds: 1 * minuteMultiplier,
+      breakTimeSeconds: 1 * minuteMultiplier,
+      sessionState: true,
+      breakState: false,
+    }
   );
-  const [isActive, setIsActive] = useState(false);
-  // current state
-  const [sessionState, setSessionState] = useState(true);
-  const [breakState, setBreakState] = useState(false);
-  // const [idleState, setIdleState] = useState(true);
 
   const toggle = () => {
-    setIsActive(!isActive);
+    dispatch({ type: "TOGGLE_PLAY_PAUSE" });
   };
 
   const reset = () => {
     // timer resets to default
-    setSession(defaultSession);
-    setBreaktime(defaultBreak);
-    setTimeSeconds(defaultSession * minuteMultiplier);
-    setIsActive(false);
+    dispatch({ type: "RESET" });
   };
 
   // function that takes in a number
@@ -33,10 +143,10 @@ function App() {
   // e.g 5min -> 300secs
   const calculateTimeLeft = () => {
     let difference = null;
-    if (sessionState) {
-      difference = timeSeconds;
-    } else if (breakState) {
-      difference = breakTimeSeconds;
+    if (state.sessionState) {
+      difference = state.timeSeconds;
+    } else if (state.breakState) {
+      difference = state.breakTimeSeconds;
     }
 
     console.log("diff: ", difference);
@@ -61,45 +171,39 @@ function App() {
     let sessionInterval = null;
     let breakInterval = null;
 
-    // should i lock in my session and breaktime here?
-    if (isActive) {
-      if (sessionState) {
+    if (state.isActive) {
+      if (state.sessionState) {
         sessionInterval = setInterval(() => {
-          if (timeSeconds > 0) {
-            setTimeSeconds((timeSeconds) => timeSeconds - 1);
-            // behind by 1 render
-            console.log("if sesh: ", timeSeconds);
-          }
-        }, 100);
-      } else if (breakState) {
-        breakInterval = setInterval(() => {
-          if (breakTimeSeconds > 0) {
-            setBreakTimeSeconds((breakTimeSeconds) => breakTimeSeconds - 1);
+          if (state.timeSeconds > 0) {
+            dispatch({ type: "SESSION_RUNNING" });
 
-            console.log("if break: ", breakTimeSeconds);
+            // behind by 1 render
+            console.log("if sesh: ", state.timeSeconds);
           }
-        }, 100);
+        }, 1000);
+      } else if (state.breakState) {
+        breakInterval = setInterval(() => {
+          if (state.breakTimeSeconds > 0) {
+            dispatch({ type: "BREAK_RUNNING" });
+
+            console.log("if break: ", state.breakTimeSeconds);
+          }
+        }, 1000);
       }
     }
-    // && !isActive
-    if (timeSeconds === 0) {
+    if (state.timeSeconds === 0) {
       // hold for a second setTimeout?
       setTimeout(() => {
         console.log("timeout");
-        // alternate states:
-        setSessionState(false);
-        setBreakState(true);
+        dispatch({ type: "SESSION_BREAK" });
         // clearInterval(sessionInterval);
-        setTimeSeconds(session * minuteMultiplier);
       }, 1000);
-      console.log("else sesh: ", timeSeconds);
-    } else if (breakTimeSeconds === 0) {
+      console.log("else sesh: ", state.timeSeconds);
+    } else if (state.breakTimeSeconds === 0) {
       setTimeout(() => {
-        console.log("else break: ", breakTimeSeconds);
-        setBreakState(false);
-        setSessionState(true);
+        console.log("else break: ", state.breakTimeSeconds);
+        dispatch({ type: "BREAK_SESSION" });
         clearInterval(breakInterval);
-        setBreakTimeSeconds(breaktime * minuteMultiplier);
       }, 1000);
     }
 
@@ -107,7 +211,13 @@ function App() {
       clearInterval(sessionInterval);
       clearInterval(breakInterval);
     };
-  }, [isActive, timeSeconds, breakTimeSeconds, sessionState, breakState]);
+  }, [
+    state.isActive,
+    state.timeSeconds,
+    state.breakTimeSeconds,
+    state.sessionState,
+    state.breakState,
+  ]);
 
   return (
     <div className="App">
@@ -122,11 +232,10 @@ function App() {
             onClick={() => {
               // if the new state is computed using the prev state, you can pass a function to setState.
               // The function will receive the previous value and return an updated value.
-              if (session > 59) {
-                setSession(60);
+              if (state.session > 59) {
+                dispatch({ type: "SESSION_OVERFLOW" });
               } else {
-                setSession((session) => session + 1);
-                setTimeSeconds((timeSeconds) => timeSeconds + 60);
+                dispatch({ type: "INC_SESSION" });
               }
             }}
           >
@@ -136,18 +245,19 @@ function App() {
           <button
             id="session-decrement"
             onClick={() => {
-              if (session < 2) {
-                setSession(1);
+              if (state.session < 2) {
+                dispatch({ type: "SESSION_UNDERFLOW" });
               } else {
-                setSession((session) => session - 1);
-                setTimeSeconds((timeSeconds) => timeSeconds - 60);
+                dispatch({ type: "DEC_SESSION" });
               }
             }}
           >
             -
           </button>
           {/* 6 */}
-          <div id="session-length">{session}</div>
+          <div id="session-length">
+            {state.session ? state.session : defaultSession}
+          </div>
         </div>
         <div className="break-container">
           {/* Story 1 */}
@@ -156,13 +266,10 @@ function App() {
           <button
             id="break-increment"
             onClick={() => {
-              if (breaktime > 59) {
-                setBreaktime(60);
+              if (state.breaktime > 59) {
+                dispatch({ type: "BREAK_OVERFLOW" });
               } else {
-                setBreaktime((breaktime) => breaktime + 1);
-                setBreakTimeSeconds(
-                  (breakTimeSeconds) => breakTimeSeconds + 60
-                );
+                dispatch({ type: "INC_BREAK" });
               }
             }}
           >
@@ -172,24 +279,25 @@ function App() {
           <button
             id="break-decrement"
             onClick={() => {
-              if (breaktime < 2) {
-                setBreaktime(1);
+              if (state.breaktime < 2) {
+                dispatch({ type: "BREAK_UNDERFLOW" });
               } else {
-                setBreaktime((breaktime) => breaktime - 1);
-                setBreakTimeSeconds(
-                  (breakTimeSeconds) => breakTimeSeconds - 60
-                );
+                dispatch({ type: "DEC_BREAK" });
               }
             }}
           >
             -
           </button>
           {/* 5  */}
-          <div id="break-length">{breaktime}</div>
+          <div id="break-length">
+            {state.breaktime ? state.breaktime : defaultBreak}
+          </div>
         </div>
         <div className="timer-container">
           {/* 7 */}
-          <p id="timer-label">{`${sessionState ? "SESSION" : "BREAK"}`}</p>
+          <p id="timer-label">{`${
+            state.isActive ? (state.sessionState ? "SESSION" : "BREAK") : "IDLE"
+          }`}</p>
           {/* 8 */}
           <div id="time-left">
             {calculateTimeLeft().minutes < 10
@@ -202,7 +310,7 @@ function App() {
           </div>
           {/* 9 */}
           <button id="start_stop" onClick={toggle}>
-            {`${isActive ? "PAUSE" : "PLAY"}`}
+            {`${state.isActive ? "PAUSE" : "PLAY"}`}
           </button>
           {/* 10 */}
           <button id="reset" onClick={reset}>
